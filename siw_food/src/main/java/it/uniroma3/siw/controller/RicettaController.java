249d39ad32bc04c281958322bd18c6e59fdc785f
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-06-26 20:26:11 +0200

    Implement all 3 /elenco

--- siw_food/src/main/java/it/uniroma3/siw/controller/RicettaController.java
+++ siw_food/src/main/java/it/uniroma3/siw/controller/RicettaController.java
@@ -2,8 +2,12 @@ package it.uniroma3.siw.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import it.uniroma3.siw.controller.validation.RicettaValidator;
+import it.uniroma3.siw.model.Ricetta;
 import it.uniroma3.siw.service.RicettaService;
 
 @Controller
@@ -22,5 +26,27 @@ public class RicettaController {
 	
 	@Autowired
 	private RicettaValidator ricettaValidator;
+	
+	/*##############################################################*/
+	/*###########################METHODS############################*/
+	/*##############################################################*/
+//======================================================================\\
+
+	/*##############################################################*/
+	/*###########################/ricette###########################*/
+	/*##############################################################*/
+
+	@GetMapping("/elencoRicette")
+	public String showelencoRicette(Model model) {
+		Iterable<Ricetta> allRicette = this.ricettaService.findAll();
+		model.addAttribute("allRicette", allRicette);
+		return "elencoRicette.html";
+	}
+
+	@GetMapping("/ricetta/{id}")
+	public String showRicetta(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("ricetta", this.ricettaService.findById(id));
+		return "ricetta.html";
+	}
 	
 }
--- siw_food/src/main/java/it/uniroma3/siw/service/CuocoService.java
+++ siw_food/src/main/java/it/uniroma3/siw/service/CuocoService.java
@@ -3,6 +3,7 @@ package it.uniroma3.siw.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.uniroma3.siw.model.Cuoco;
 import it.uniroma3.siw.repository.CuocoRepository;
 
 @Service
@@ -14,10 +15,18 @@ public class CuocoService {
 	
 	@Autowired
 	private CuocoRepository cuocoRepository;
-	
+
+
 	/*##############################################################*/
 	/*###########################METHODS############################*/
 	/*##############################################################*/
 	
+	public Iterable<Cuoco> findAll() {
+		return this.cuocoRepository.findAll();
+	}
+
+	public Cuoco findById(Long id) {
+		return this.cuocoRepository.findById(id).get();
+	}
 	
 }
