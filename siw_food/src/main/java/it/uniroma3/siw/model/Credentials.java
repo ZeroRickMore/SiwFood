64333ba43b9c01a1f871b7846813d5d3c35bb964
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-06-26 13:49:31 +0200

    Add validate annotations

---
+++ siw_food/src/main/java/it/uniroma3/siw/model/Credentials.java
@@ -0,0 +1,88 @@
+package it.uniroma3.siw.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Credentials {
+	
+	/*##############################################################*/
+	/*######################RUOLI STATICI###########################*/
+	/*##############################################################*/
+	
+	public static final String EDITORE_ROLE = "EDITORE";
+	public static final String ADMIN_ROLE = "ADMIN";
+	
+	/*##############################################################*/
+	/*#########################VARIABLES############################*/
+	/*##############################################################*/
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@NotBlank
+	private String username;
+	
+	@NotBlank
+	private String password;
+	
+	private String role;
+	
+	@NotNull
+	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private User user;
+	
+	/*##############################################################*/
+	/*####################GETTERS AND SETTERS#######################*/
+	/*##############################################################*/
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public static String getEditoreRole() {
+		return EDITORE_ROLE;
+	}
+	public static String getAdminRole() {
+		return ADMIN_ROLE;
+	}
+	
+	/*##############################################################*/
+	/*#######################CLASS METHODS##########################*/
+	/*##############################################################*/
+	
+}
--- siw_food/src/main/java/it/uniroma3/siw/model/User.java
+++ siw_food/src/main/java/it/uniroma3/siw/model/User.java
@@ -1,15 +1,15 @@
 package it.uniroma3.siw.model;
 
-import java.util.Objects;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
-public class Ingrediente {
+@Table(name = "users")
+public class User {
 	
 	/*##############################################################*/
 	/*#########################VARIABLES############################*/
@@ -22,7 +22,8 @@ public class Ingrediente {
 	@NotBlank
 	private String nome;
 	
-	private String image_path;
+	@NotBlank
+	private String cognome;
 	
 	/*##############################################################*/
 	/*####################GETTERS AND SETTERS#######################*/
@@ -31,44 +32,24 @@ public class Ingrediente {
 	public Long getId() {
 		return id;
 	}
-	public String getNome() {
-		return nome;
-	}
-	public String getImage_path() {
-		return image_path;
-	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+	public String getNome() {
+		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public void setImage_path(String image_path) {
-		this.image_path = image_path;
+	public String getCognome() {
+		return cognome;
 	}
-
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
 	/*##############################################################*/
-	/*#####################EQUALS, HASHCODE#########################*/
+	/*#######################CLASS METHODS##########################*/
 	/*##############################################################*/
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(nome);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Ingrediente other = (Ingrediente) obj;
-		return Objects.equals(nome, other.nome);
-	}
-	
-	
-	
-	
 	
 }
