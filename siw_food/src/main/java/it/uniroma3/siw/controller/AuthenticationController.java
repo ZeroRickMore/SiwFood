71066bb9cd2530bfc8db224f266dcb3048de69c3
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-06-29 21:39:28 +0200

    Implement duplicate user check on registration and error throw

--- siw_food/src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
+++ siw_food/src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
@@ -5,16 +5,20 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import it.uniroma3.siw.controller.validation.CredentialsValidator;
 import it.uniroma3.siw.model.Credentials;
 import it.uniroma3.siw.model.User;
 import it.uniroma3.siw.service.CredentialsService;
 import it.uniroma3.siw.service.UserService;
+import jakarta.validation.Valid;
 
 @Controller
 public class AuthenticationController {
@@ -22,17 +26,20 @@ public class AuthenticationController {
 	/*##############################################################*/
 	/*##########################SERVICES############################*/
 	/*##############################################################*/
-	
+
 	@Autowired
 	private CredentialsService credentialsService;
 
 	@Autowired
 	private UserService userService;
 
+	@Autowired
+	private CredentialsValidator credentialsValidator;
+
 	/*##############################################################*/
 	/*##########################REGISTER############################*/
 	/*##############################################################*/
-	
+
 	@GetMapping("/register")
 	public String showRegisterForm(Model model) {
 		model.addAttribute("user", new User());
@@ -40,22 +47,35 @@ public class AuthenticationController {
 		return "formRegister.html";
 	}
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@PostMapping("/register")
-	public String newUser(@ModelAttribute("user") User user, @ModelAttribute("credentials") Credentials credentials, Model model) {
+	public String newUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResultUser, 
+							@Valid @ModelAttribute("credentials") Credentials credentials, BindingResult bindingResultCredentials,
+								Model model) {
 		credentials.setUser(user);
-        credentialsService.saveCredentials(credentials); //Role lo setto qui, anche l'hash della pwd
-		return "redirect:login"; //finito di registrare redirecto a /
+		this.credentialsValidator.validate(credentials, bindingResultCredentials);
+
+		if(bindingResultUser.hasErrors() || bindingResultCredentials.hasErrors()) {
+			model.addAttribute("userErrors", bindingResultUser);
+			return "formRegister.html";
+		} else {
+			credentialsService.saveCredentials(credentials); //Role lo setto qui, anche l'hash della pwd
+			return "redirect:login"; //finito di registrare redirecto a /
+		}
+
 	}
 
 	/*##############################################################*/
 	/*############################INDEX#############################*/
 	/*##############################################################*/
-	
+
 	@GetMapping("/")
 	public String showIndex(Model model) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if(authentication instanceof AnonymousAuthenticationToken) {
-			return "index.html";
+			return "placeholder.html";
 		}
 		else {
 			UserDetails userDetails = (UserDetails)authentication.getPrincipal();
@@ -65,17 +85,17 @@ public class AuthenticationController {
 			}
 			return "index.html";
 		}
-		
+
 	}
-	
+
 	/*##############################################################*/
 	/*###########################LOGIN##############################*/
 	/*##############################################################*/
-	
+
 	@GetMapping("/login")
 	public String showLoginForm(Model model) {
 		return "login.html";
 	}
-	
+
 
 }
---
+++ siw_food/src/main/java/it/uniroma3/siw/controller/validation/CredentialsValidator.java
@@ -0,0 +1,53 @@
+package it.uniroma3.siw.controller.validation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import it.uniroma3.siw.model.Credentials;
+import it.uniroma3.siw.service.CredentialsService;
+
+@Component
+public class CredentialsValidator implements Validator {
+
+	/*##############################################################*/
+	/*#########################SERVICES#############################*/
+	/*##############################################################*/
+
+	@Autowired
+	private CredentialsService credentialsService;
+
+	/*##############################################################*/
+	/*#########################VALIDATE#############################*/
+	/*##############################################################*/
+
+	@Override
+	public void validate(Object o, Errors errors) {
+
+		Credentials credentials = (Credentials) o;
+		//USERNAME UNICO
+		if(credentials.getUsername()!=null
+				&& this.credentialsService.existsByUsername(credentials.getUsername())) {
+			System.out.println("\nCred duplicate!\n");
+			errors.reject("credentials.duplicato");
+		}
+	}
+
+
+	/*##############################################################*/
+	/*##################VALIDATE SUPPORT METHODS####################*/
+	/*##############################################################*/
+
+
+
+	/*##############################################################*/
+	/*###########################SUPPORTS###########################*/
+	/*##############################################################*/
+
+	@Override
+	public boolean supports(Class<?> aClass) {
+		return CredentialsValidator.class.equals(aClass);
+	}
+
+}
